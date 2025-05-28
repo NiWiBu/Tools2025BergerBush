408ecf0c8edb6fdf418e628f3e7f9079cf289651
Vane <s2410237005@fhooe.at>
2025-05-28 14:35:11 +0200

    bug fixed

--- Berger_Bush_android/dummy.java
+++ Berger_Bush_android/dummy.java
@@ -4,6 +4,7 @@ public class dummy {
         int i = 0;
 
         int a = 3;
+        System.out.println("bug fixed");
     }
     public static void main(String[] args) {
         System.out.println("Hello World!");

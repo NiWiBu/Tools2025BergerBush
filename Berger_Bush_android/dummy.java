1ecee116d1e2b29d0df797e6f13b56bdabca32de
Vane <s2410237005@fhooe.at>
2025-05-28 14:20:48 +0200

    added to new method

--- Berger_Bush_android/dummy.java
+++ Berger_Bush_android/dummy.java
@@ -1,8 +1,11 @@
 public class dummy {
-    public void thisIsANewMethod() { }
+    public void thisIsANewMethod() {
+        int i = 0;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("ABCD");
+
 
     }
 }

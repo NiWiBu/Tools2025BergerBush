d2b1c576cdfc6772a91a398f51087285a61453e1
NiWiBu <s2410237010@fhooe.at>
2025-05-28 14:45:59 +0200

    methodThree was added

--- Berger_Bush_android/dummy.java
+++ Berger_Bush_android/dummy.java
@@ -10,11 +10,13 @@ public class dummy {
         int a = 0;
     }
 
-    public void methodTwo(){
+    public void methodTwo() {
         int b = 1;
     }
 
-    
+    public void methodThree(){
+        int c = 2;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
